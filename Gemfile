8430f5797a6bc1d138e9d50a763e75b1fc3134ab
Andres Otarola <andres@otarola.me>
2014-01-12 02:25:26 -0300

    Added core files

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+ruby '1.9.3'
+
+gem 'couchrest', '~> 1.2'
+gem 'json', '~> 1.8.1'
+gem 'redis', '~> 3.0.6'
+gem 'hiredis', '~> 0.4.5'
---
+++ app.rb
@@ -0,0 +1,41 @@
+require 'rubygems'
+require 'couchrest'
+require 'logger'
+require 'json'
+require 'redis'
+
+redis = Redis.new :driver => :hiredis
+
+databases = redis.smembers 'speedwing:databases'
+
+logger = Logger.new 'log/log.txt', 'daily'
+
+logger.info "[INIT]"
+def listen_for_changes(database, redis, logger)
+  db = CouchRest.database("http://localhost:5984/#{database}")
+  since = redis.get "shortcircuit:#{database}:since" || 'now'
+  opts = {
+    :since => since,
+    :feed => 'continuous',
+    :heartbeat => true,
+    :include_docs => true,
+    :filter => 'app/lines'
+  }
+  db.changes opts do |payload|
+    pretty_payload = JSON.pretty_generate(payload)
+    #TODO: code that saves the payload to a data store
+    logger.info "[EVENT] #{database} : \n#{pretty_payload}"
+    redis.set "shortcircuit:#{database}:since", payload['seq']
+  end
+end
+
+threads = []
+
+databases.each do |db|
+  thread = Thread.new {listen_for_changes(db, redis, logger)}
+  threads.push thread
+end
+
+logger.info "[STARTING THREADS]"
+
+threads.map!(&:join)
