0262ca098307162ce00862edd2c08d8bde06e58c
Andres Otarola <andres@otarola.me>
2014-01-12 02:32:46 -0300

    Changed to a more sugar syntax style :)

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ logger = Logger.new 'log/log.txt', 'daily'
 logger.info "[INIT]"
 def listen_for_changes(database, redis, logger)
   db = CouchRest.database("http://localhost:5984/#{database}")
-  since = redis.get "shortcircuit:#{database}:since" || 'now'
+  since = redis.get "shortcircuit:#{database}:since" or 'now'
   opts = {
     :since => since,
     :feed => 'continuous',
